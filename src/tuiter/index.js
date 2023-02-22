86f7c49c68e5b23f23992ff6866ffe1a7588273c
parthamv <mvgopesh@gmail.com>
2023-02-22 16:29:03 -0500

    a6 Initial Changes

---
+++ src/tuiter/index.js
@@ -0,0 +1,16 @@
+import Nav from "../nav";
+
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList
+  from "./who-to-follow-list";
+function Tuiter() {
+ return (
+   <div>
+     <Nav/>
+     <NavigationSidebar active="home"/>
+     <WhoToFollowList/>
+     <h1>Tuiter</h1>
+   </div>
+ );
+}
+export default Tuiter
