613ba42f4295443ac455c1c377c12e2136338520
parthamv <mvgopesh@gmail.com>
2023-03-29 20:11:51 -0400

    changes6

--- src/tuiter/home/whats-happening.js
+++ src/tuiter/home/whats-happening.js
@@ -14,6 +14,10 @@ const WhatsHappening = () => {
       dispatch(createTuitThunk(newTuit));
       window.location.reload();
  }
+
+
+
+
  return (
    <div className="row">
      <div className="col-auto">
